9bd240d0a7cc52eb53d95233dfcf8a8a8583e3cd
Manisha148 <70441355+Manisha148@users.noreply.github.com>
2023-04-04 10:56:02 +0530

    Update main.tf

--- tf_aws_pipe-main/main.tf
+++ tf_aws_pipe-main/main.tf
@@ -1,42 +1,74 @@
-locals {
-  bucketName=var.BucketName
-}
-provider "aws" {
-  region = "us-east-1"
-  shared_credentials_files = ["/credentials"]
-  profile = "demo"
-}
-
-resource "aws_s3_bucket" "bucket" {
-  bucket = local.bucketName
-  versioning {
-    enabled=true
-  }
-  
-  acl = "private"
+# EC2 Instance
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
   tags = {
-    Name        = "My bucket"
-    Environment = "test"
+    Name = "example"
   }
 }
-resource "aws_s3_bucket_lifecycle_configuration" "bucket-config" {
-  bucket=aws_s3_bucket.bucket.bucket
-  rule {
-    id = "demorule"
 
-    filter {}
+# S3 Bucket
+resource "aws_s3_bucket" "example" {
+  bucket = "example-bucket"
+  acl    = "private"
 
-    transition {
-      days          = 30
-      storage_class = "STANDARD_IA"
-    }
-
-    transition {
-      days          = 60
-      storage_class = "GLACIER"
-    }
-
-    status = "Enabled"
+  tags = {
+    Name = "example"
   }
-    
+}
+
+# VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "example"
+  }
+}
+
+# Security Group
+resource "aws_security_group" "example" {
+  name_prefix = "example-sg"
+  vpc_id      = aws_vpc.example.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "example"
+  }
+}
+
+# Buildspec file for AWS CodePipeline
+resource "aws_codebuild_project" "example" {
+  name          = "example-project"
+  description   = "Example CodeBuild project"
+  build_timeout = 60
+
+  environment {
+    compute_type = "BUILD_GENERAL1_SMALL"
+    image        = "aws/codebuild/standard:5.0"
+    type         = "LINUX_CONTAINER"
+  }
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  source {
+    type            = "CODEPIPELINE"
+    buildspec       = file("buildspec.yml")
+    report_build_status = true
+  }
 }
